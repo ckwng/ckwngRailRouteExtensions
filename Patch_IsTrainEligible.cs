731359cf89e9529b6997b11675c54d5626ed106b
ckwng <calkwng@gmail.com>
2021-12-15 20:38:56 -0800

    0.2.0
    Enable departure sensor finished route for freights
    Mark trains to be disposed as ineligible for automation
    Package Harmony in same folder as mod DLL
    Other clean up

---
+++ Patch_IsTrainEligible.cs
@@ -0,0 +1,20 @@
+﻿using Game.Railroad.Sensor;
+using Game.Train;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ckwng.RailRoute
+{
+    [HarmonyPatch(typeof(AutoSignalSensor), "IsTrainEligible")]
+     class Patch_IsTrainEligible
+    {
+        public static bool Postfix(bool result, Train train)
+        {
+            return result ? !train.Head.ShallDisposeTrain(train) : result;
+        }
+    }
+}
---
+++ Patch_TriggerAutomation.cs
@@ -0,0 +1,51 @@
+﻿using Game.Railroad.Sensor;
+using Game.Train;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ckwng.RailRoute
+{
+    [HarmonyPatch(typeof(TwoSignalsSensor), "TriggerAutomation")]
+    class Patch_TriggerAutomation
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var finishedScheduleRouteField = AccessTools.Field(typeof(StationSelector), "FinishedScheduleRoute");
+            var trainTypeGetter = AccessTools.PropertyGetter(typeof(Train), "TrainType");
+            var foundFinishedScheduleRoute = false;
+            var complete = false;
+            foreach (var instruction in instructions)
+            {
+                if (!complete)
+                {
+                    if (foundFinishedScheduleRoute)
+                    {
+                        if (instruction.Calls(trainTypeGetter))
+                        {
+                            complete = true; //start yielding again starting from the next instruction
+                        }
+                        continue; //don't yield the instruction
+                    }
+                    else if (instruction.LoadsField(finishedScheduleRouteField)) {
+                        foundFinishedScheduleRoute = true;
+                    }
+                }
+                yield return instruction;
+            }
+            if (!complete)
+            {
+                Debug.Log("[ckwng] Failed to transpile TriggerAutomation().");
+            }
+            else
+            {
+                Debug.Log("[ckwng] Transpiled TriggerAutomation().");
+            }
+        }
+    }
+}
